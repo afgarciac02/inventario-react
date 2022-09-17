02218594c86021cbcf5ad7c0a4b057518df1810d
afgarciac02 <afgarciac02@libertadores.edu.co>
2022-09-17 09:16:46 -0500

    implementacion bootstrap

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
+import BootstrapStyleSheet from 'react-native-bootstrap-styles';
 import {
   StyleSheet,
   Text,
@@ -9,41 +10,49 @@ import {
   Button,
   TouchableOpacity,
 } from "react-native";
+
+const bootstrapStyleSheet = new BootstrapStyleSheet();
+const {s, c} = bootstrapStyleSheet;
  
 export default function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
  
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Email."
-          placeholderTextColor="#003f5c"
-          onChangeText={(email) => setEmail(email)}
-        />
-      </View>
- 
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Password."
-          placeholderTextColor="#003f5c"
-          secureTextEntry={true}
-          onChangeText={(password) => setPassword(password)}
-        />
-      </View>
- 
-      <TouchableOpacity>
-        <Text style={styles.forgot_button}>Forgot Password?</Text>
-      </TouchableOpacity>
- 
-      <TouchableOpacity style={styles.loginBtn}>
-        <Text style={styles.loginText}>LOGIN</Text>
-      </TouchableOpacity>
+
+    <View style={[s.body]}>
+        <Text style={[s.text, s.textPrimary]}>funcionando sin bootstrap</Text>
     </View>
+
+    // <View style={styles.container}>
+    //   <StatusBar style="auto" />
+    //   <View style={styles.inputView}>
+    //     <TextInput
+    //       style={styles.TextInput}
+    //       placeholder="Email."
+    //       placeholderTextColor="#003f5c"
+    //       onChangeText={(email) => setEmail(email)}
+    //     />
+    //   </View>
+ 
+    //   <View style={styles.inputView}>
+    //     <TextInput
+    //       style={styles.TextInput}
+    //       placeholder="Password."
+    //       placeholderTextColor="#003f5c"
+    //       secureTextEntry={true}
+    //       onChangeText={(password) => setPassword(password)}
+    //     />
+    //   </View>
+ 
+    //   <TouchableOpacity>
+    //     <Text style={styles.forgot_button}>Forgot Password?</Text>
+    //   </TouchableOpacity>
+ 
+    //   <TouchableOpacity style={styles.loginBtn}>
+    //     <Text style={styles.loginText}>LOGIN</Text>
+    //   </TouchableOpacity>
+    // </View>
   );
 }
  
